e2906cd648c1df495f26799bdb8e905f9576f1ba
Muhammad-Nuraliyev <muhammad.nuraliyev208@gmail.com>
2025-03-11 06:57:51 +0500

    next-texnomart

--- src/app/top-menu/[slug]/page.tsx
+++ src/app/top-menu/[slug]/page.tsx
@@ -12,9 +12,12 @@ function TopMenu() {
   const [topMenu, setTopMenu] = useState<TopMenuType | null>(null);
   const { slug } = useParams();
   const [currentPage, setCurrentPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!slug) return;
+
+    setLoading(true);
 
     axios
       .get(
@@ -25,16 +28,27 @@ function TopMenu() {
 
         if (response.data && response.data.data) {
           setTopMenu(response.data.data);
+        } else {
+          setTopMenu(null);
         }
+      })
+      .catch((error) => {
+        console.error("Xatolik yuz berdi:", error);
+        setTopMenu(null);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   }, [slug, currentPage]);
 
-  if (!slug || !topMenu) return <p>Loading...</p>;
+  if (!slug) return <p>Loading...</p>;
 
   return (
     <div className="px-10 mx-auto my-6">
       <div className="grid grid-cols-5 gap-6 container px-10 mx-auto mb-4">
-        {topMenu.products && topMenu.products.length > 0 ? (
+        {loading ? (
+          <p>Yuklanmoqda...</p>
+        ) : topMenu && topMenu.products && topMenu.products.length > 0 ? (
           topMenu.products.map((item) => (
             <Link href={`/product/${item.id}`} key={item.id}>
               <CardPage item={item} />
@@ -44,10 +58,15 @@ function TopMenu() {
           <p>Mahsulotlar topilmadi</p>
         )}
       </div>
+
       <Pagination
         current={currentPage}
-        total={topMenu.total || 0}
-        onChange={(page) => setCurrentPage(page)}
+        total={topMenu?.total || 0}
+        pageSize={20}
+        onChange={(page) => {
+          setCurrentPage(page);
+          setTopMenu(null);
+        }}
       />
     </div>
   );
